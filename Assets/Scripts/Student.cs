d51bec013e8cec92ff9fd24a6a3bcbf1c8b45066
Jason Jaslow <jason@jaslow.com>
2020-12-29 23:20:59 -0500

    Imported models, built student and teacher prefabs

---
+++ Assets/Scripts/Student.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Student : MonoBehaviour
+{
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1,
+        Other = 3
+    }
+
+    string studentName;
+    int studentIDNumber;
+    Gender studentGender;
+    Color avatarColor;
+
+    int gradeAttending;
+    float currentGPA;
+
+
+    #region Getters
+    public string GetStudentName()
+    {
+        return studentName;
+    }
+    public int GetStudentID()
+    {
+        return studentIDNumber;
+    }
+    public Gender GetStudentGender()
+    {
+        return studentGender;
+    }
+    public Color GetAvatarColor()
+    {
+        return avatarColor;
+    }
+    public int GetGradeAttending()
+    {
+        return gradeAttending;
+    }
+    public float GetStudentGPA()
+    {
+        return currentGPA;
+    }
+    #endregion
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Teacher : MonoBehaviour
+{
+    string teacherName;
+    string subjectTeaching;
+    string classCode;
+
+
+    #region Getters
+    public string GetTeacherName()
+    {
+        return teacherName;
+    }
+    public string GetSubjectTeaching()
+    {
+        return subjectTeaching;
+    }
+    public string GetClassCode()
+    {
+        return classCode;
+    }
+    #endregion
+
+
+
+
+
+}
